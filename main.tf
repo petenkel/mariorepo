d9c3afcdabbca64acf5aacd777845fe906b0a7d1
petenkel <peterson.soares@enkel.com.br>
2019-03-05 13:12:13 -0300

    commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+
+#Baixar a imagem do projeto docker-supermario
+resource "docker_image" "image_id" {
+    name = "pengbai/docker-supermario:latest"
+}
+
+# Inicia Container
+resource "docker_container" "container_id" {
+    name  = "supermario"
+    image = "${docker_image.image_id.latest}"
+    ports {
+         internal = "8080"
+        external = "80"
+    }
+}
+# informar o IP e nome do container
+output "Endereco IP" {
+    value = "${docker_container.container_id.ip_address}"
+    }
+output "Nome do Container" {
+    value = "${docker_container.container_id.name}"
+}
